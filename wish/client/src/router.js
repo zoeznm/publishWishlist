06a964a3672427acf96f9e99054ea48fc791254f
zoeznm <bomikim046@gmail.com>
2024-10-11 10:23:47 +0900

    save

--- wish/client/src/router.js
+++ wish/client/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Main from './components/Main.vue';
 import Top from './views/Top.vue';
+import Bottom from './views/Bottom.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/top',
     name: 'TopComponent',
     component: Top
+  },
+  {
+    path: '/bottom',
+    name: 'BottomComponent',
+    component: Bottom
   },
   // 다른 라우트 추가 가능
 ];
---
+++ wish/client/src/views/Bottom.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class="shooting-stars">
+      <div class="scope bigger">
+        <div class="crosshair-vertical"></div>
+        <div class="crosshair-horizontal"></div>
+        <div class="center-dot"></div>
+        <div id="random-points"></div>
+      </div>
+    </div>
+    <h1 class="neon-text blinking">Hit the target!</h1>
+    <a href="/main" class="back">Back</a>
+    <div class="info-box" id="info-box"></div>
+    <div class="controls">
+      <button id="up" class="game-button">▲</button>
+      <button id="left" class="game-button">◀</button>
+      <button id="down" class="game-button">▼</button>
+      <button id="right" class="game-button">▶</button>
+      <button id="restart" class="restart-button">re</button>
+      <button id="save" class="save-button">save</button>
+    </div>
+    <div class="saved-boxes-container" id="saved-boxes-container"></div>
+</template>
+
+<script>
+export default {
+  name: 'BottomComponent',
+  mounted() {
+    import('@/assets/js/bottom.js');
+  }
+};
+</script>
+
+<style src="@/assets/css/bottom.css"></style>
